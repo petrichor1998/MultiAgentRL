b48db609767758a17e713967b2feed635e2d3121
Alakh Aggarwal <alakh1997@gmail.com>
2021-11-29 13:34:52 -0600

    Update Q-Learning with operators

--- gym-bank_world/gym_bank_world/Qlearning.py
+++ gym-bank_world/gym_bank_world/Qlearning.py
@@ -19,27 +19,31 @@ test_epsilon = 0.01
 
 
 
-def train(train_env,action_space= 5, num_episodes = 1000, q_path = None):
+def train(train_env, action_space= 5, num_episodes = 1000, q_path = None):
     # create lists to contain total rewards and steps per episode
     episode_rewards = []
     random_ep = np.arange(0.01, 0.75, 0.99 / num_episodes, dtype=float)[::-1]
     steps = 0
     i = 0
 
-    Q_table = {}
+    QPickup = {}
+    QDrop = {}
+    Q_table = [QPickup, QDrop]
     start_episode = 0
     if q_path is not None:
         start_episode = int(q_path.split('_')[-1].split('.')[0])
         Q_table = pkl.load(open(q_path, "rb"))
     for episode in range(start_episode, num_episodes):
+        
         # Reset environment
         train_env.reset()
         # train_env.render()
         ep = random_ep[min(i, random_ep.shape[0] - 1)]
         i += 1
 
-        # Get state representation
-        s = get_state_representation(train_env.obs_dict, train_env.no_of_agents, train_env.no_of_gems)
+        # # Get state representation
+        # s = get_state_representation(train_env.obs_dict, train_env.no_of_agents, train_env.no_of_gems)
+        s = train_env.obs_dict
         while not train_env.done:
 
             # Get action
@@ -49,12 +53,12 @@ def train(train_env,action_space= 5, num_episodes = 1000, q_path = None):
             train_env.step(a_list)
             # train_env.render()
 
-            # Get new state
-            s_prime = get_state_representation(train_env.obs_dict, train_env.no_of_agents, train_env.no_of_gems)
-
+            # # Get new state
+            # s_prime = get_state_representation(train_env.obs_dict, train_env.no_of_agents, train_env.no_of_gems)
+            s_prime = train_env.obs_dict
 
             # update Q_table
-            update_qvalue(Q_table, action_space, s, a_list, train_env.total_reward,  s_prime, train_env.no_of_agents)
+            update_qvalue(Q_table, action_space, s, a_list, train_env.total_reward, s_prime, train_env.no_of_agents)
 
             s = s_prime
 
@@ -62,54 +66,84 @@ def train(train_env,action_space= 5, num_episodes = 1000, q_path = None):
         print(f"Episode {episode + 1} reward: ", train_env.total_reward)
         plt.clf()
         plt.plot(episode_rewards)
-        plt.savefig("D:\RL_project\Episode_reward_plot.png")
+        plt.savefig("C:\\Users\\alakh\\OneDrive\\Desktop\\Multi_Agent_RL-main\\plot.png")
         if (episode+1)%10 == 0:
-            with open(f"D:\RL_project\QTable_{episode+1}.pkl", "wb") as fi:
+            with open(f"C:\\Users\\alakh\\OneDrive\\Desktop\\Multi_Agent_RL-main\\QTable_{episode+1}.pkl", "wb") as fi:
                 pkl.dump(Q_table, fi)
 
     cv2.destroyAllWindows()
     return Q_table, episode_rewards
 
 
-def update_qvalue(Q_table, action_space, s, a_list, r, s_prime, no_of_agents):
-    if s_prime in Q_table.keys():
-        q_next = Q_table[s_prime]
-    else:
-        q_next = np.zeros((no_of_agents, action_space))
-        Q_table[s_prime] = q_next
-    if s not in Q_table.keys():
-        Q_table[s] = np.zeros((no_of_agents, action_space))
+def update_qvalue(Q_table, action_space, obs, a_list, r, obs_prime, no_of_agents):
+    for i in range(no_of_agents):
+        a = a_list[i]
+        if obs[f"A_{i}"][1] == -1:
+            s_prime = get_state_representation(obs_prime, i, 'pick')
+            s = get_state_representation(obs, i, 'pick')
+            if s_prime in Q_table[0].keys():
+                q_next = Q_table[0][s_prime]
+            else:
+                q_next = np.zeros(action_space)
+                Q_table[0][s_prime] = q_next
+            if s not in Q_table[0].keys():
+                Q_table[0][s] = np.zeros(action_space)
 
-    for i, a in enumerate(a_list):
-        Q_table[s][i][a] = Q_table[s][i][a] + learning_rate * (r + discount_factor * np.max(q_next[i]) - Q_table[s][i][a])
+            Q_table[0][s][a] = Q_table[0][s][a] + learning_rate * (r + np.max(q_next) - Q_table[0][s][a])
+        else:
+            s_prime = get_state_representation(obs_prime, i, 'drop')
+            s = get_state_representation(obs, i, 'drop')
+            if s_prime in Q_table[1].keys():
+                q_next = Q_table[1][s_prime]
+            else:
+                q_next = np.zeros(action_space)
+                Q_table[1][s_prime] = q_next
+            if s not in Q_table[1].keys():
+                Q_table[1][s] = np.zeros(action_space)
+
+            Q_table[1][s][a] = Q_table[1][s][a] + learning_rate * (r + np.max(q_next) - Q_table[1][s][a])
 
 
-def get_action(Q_table, action_space, ep, s, no_of_agents):
+def get_action(Q_table, action_space, ep, obs, no_of_agents):
     if np.random.randn(0, 1) < ep:
         action_list = [random.randint(0, action_space - 1) for i in range(no_of_agents)]
 
     else:
-        if s in Q_table:
-            action_list = []
-            for i in range(no_of_agents):
-                where_max = np.where(Q_table[s][i] == np.max(Q_table[s][i]))[0]
-                if len(where_max) == 1:
-                    a = where_max[0]
+        action_list = []
+        Q_pick = Q_table[0]
+        Q_drop = Q_table[1]
+        for i in range(no_of_agents):
+            if obs[f"A_{i}"][1] == -1:
+                s = get_state_representation(obs, i, 'pick')
+                if s in Q_pick:
+                    where_max = np.where(Q_pick[s] == np.max(Q_pick[s]))[0]
+                    if len(where_max) == 1:
+                        a = where_max[0]
+                    else:
+                        a = np.random.choice(where_max)
+                    action_list.append(a)
                 else:
-                    a = np.random.choice(where_max)
-                action_list.append(a)
-        else:
-            action_list = [random.randint(0, action_space - 1) for i in range(no_of_agents)]
+                    action_list.append(random.randint(0, action_space - 1))
+            else:
+                s = get_state_representation(obs, i, 'drop')
+                if s in Q_drop:
+                    where_max = np.where(Q_drop[s] == np.max(Q_drop[s]))[0]
+                    if len(where_max) == 1:
+                        a = where_max[0]
+                    else:
+                        a = np.random.choice(where_max)
+                    action_list.append(a)
+                else:
+                    action_list.append(random.randint(0, action_space - 1))
     return action_list
 
-def get_state_representation(s, no_of_agents, no_of_gems):
-    rep = ""
-    for i in range(no_of_agents):
-        rep += np.array2string(s[f"A_{i}"][0]) + "," + str(s[f"A_{i}"][1]) + "," + str(s[f"A_{i}"][2]) + ";"
-    rep += "_"
-    for j in range(no_of_gems):
-        rep += np.array2string(s[f"Gem_{j}"][0]) + "," + str(s[f"Gem_{j}"][1]) + "," + str(s[f"Gem_{j}"][2]) + ";"
 
+def get_state_representation(obs, i, task):
+    rep = np.array2string(obs[f"A_{i}"][0])+";"
+    if task == 'drop':
+        return rep
+    j = obs[f"A_{i}"][2]
+    rep += np.array2string(obs[f"Gem_{j}"][0])+";"
     return rep
 
 
